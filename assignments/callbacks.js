641f5f2c964e2941bf16267b9a23134948ca716d
Katrina Roaix <kroaix@gmail.com>
2019-05-24 23:43:50 -0400

    Closures and Callbacks done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,55 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, (length) => {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[3]);
 }
+
+last(items, (lastItem) => {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+sumNums(5, 10, (product) => {
+  console.log(product);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(5, 10, (product) => {
+  console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.forEach(function (items){
+    if (list === item){
+      return cb(true);
+    }
+  });
+  return cb(false);
 }
+
+contains('Pencil', items, (result) => {
+  console.log(result);
+});
+
 
 /* STRETCH PROBLEM */
 
